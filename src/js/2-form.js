4cfbfba1db8b5a1335ebd80b1b4278e525c19ef6
DaniJLa <klepov.bogdan@gmail.com>
2025-10-17 00:25:11 +0300

    done all tasks

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,45 @@
-console.log('Form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', handlerInput);
+form.addEventListener('submit', handlerSubmit);
+
+function handlerInput(e) {
+  formData.email = input.value.trim();
+  formData.message = textarea.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  if (formData.email && formData.message) {
+    console.log(formData);
+    formData.email = '';
+    formData.message = '';
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  } else {
+    return alert('Fill please all fields');
+  }
+}
+
+function handlerPopulate() {
+  const message = localStorage.getItem(STORAGE_KEY);
+  const messageObj = JSON.parse(message);
+  if (message) {
+    input.value = messageObj.email;
+    textarea.value = messageObj.message;
+    formData.email = messageObj.email;
+    formData.message = messageObj.message;
+  }
+}
+
+handlerPopulate();
